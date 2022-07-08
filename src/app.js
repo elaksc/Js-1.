072997c7b5b065ff746eae7cce4d8e2f5fb5dc49
elakascioglu <77547429+elaksc@users.noreply.github.com>
2022-07-08 14:35:10 +0300

    deneme

---
+++ src/app.js
@@ -0,0 +1,18 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11
+console.log(euroDun)
+
+let konutKredisi = ["Konut Kredisi", "Emlak Kredisi","Kamu Konut Kredisi"]
+console.log("<ul>")
+for (let i = 0; i <konutKredisi.length; i++) {
+    console.log("<li>" + konutKredisi[i] + "</li>")
+}
+console.log("</ul>")
